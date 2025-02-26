ea9932a174ed5aba5805883567141171f7bd67f3
Your Name <you@example.com>
2025-02-26 03:08:42 -0600

    redo

--- crypto dashboard/server/src/config/connection.js
+++ crypto dashboard/server/src/config/connection.js
@@ -4,10 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'localhost',
+    host: 'localhost',
     dialect: 'postgres',
-    port: process.env.DB_PORT || 5432,
-    logging: false,
 });
 
 export default sequelize;
