241df2ed4ed00ce45c476a099684a6485bdb3932
aswans3 <swansonandy909@gmail.com>
2025-02-26 18:12:50 -0800

    style 3

--- crypto dashboard/client/src/App.jsx
+++ crypto dashboard/client/src/App.jsx
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CryptoMarket from './routes/api/CryptoMarket';
 import StockMarket from './routes/api/StockMarket';
 import Navbar from './components/Navbar';
-import LoginModal from './components/LoginModal';
+import LoginModal from "./components/LoginModal";
 import RegisterModal from './components/RegistrationModal';
-import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import backgroundImage from './images/IMG_4115.jpeg';
 
 function Home() {
-  return <h1>Welcome to the Crypto and Stock Dashboard</h1>;
+  return <h1>Welcome to MoneyMap</h1>;
 }
 
 function App() {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isRegisterOpen, setIsRegisterOpen] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // State to manage authentication status
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLoginSuccess = () => {
     setIsAuthenticated(true);
@@ -28,8 +28,12 @@ function App() {
         onLoginClick={() => setIsLoginOpen(true)}
         onRegisterClick={() => setIsRegisterOpen(true)}
       />
+      <div 
+        className="background"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
       <div className="App p-8">
-        {!isAuthenticated && <h1 className="text-3xl mb-6">Welcome to Stock & Crypto Watcher</h1>}
+        {!isAuthenticated && <h1 className="text-3xl mb-6 border-welcome bg-white">Welcome to Stock & Crypto Watcher</h1>}
 
         <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} onLoginSuccess={handleLoginSuccess} />
         <RegisterModal isOpen={isRegisterOpen} onClose={() => setIsRegisterOpen(false)} />
